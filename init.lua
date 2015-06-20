fab55d84fac8124e3f4c943163f389ddfcdddcc4
bobombolo <danjor@halfstrong.com>
2015-06-19 18:50:26 -0600

    trying to read the bytes

--- init.lua
+++ init.lua
@@ -4,14 +4,11 @@ local ImageSize = dofile(minetest.get_modpath("realterrain").."/lua-imagesize-1.
 local demfilename = minetest.get_modpath("realterrain").."/dem/"..DEM
 local width, length, format = ImageSize.imgsize(demfilename)
 -- middle of our image
-local center_x, center_z, demtiff --used by get_pixel()
+local demtiff --used by get_pixel()
 
 if width and length then
 	demtiff = io.open(minetest.get_modpath("realterrain").."/dem/"..DEM, "rb")
-	center_x = math.ceil(width / 2)
-	center_z = math.ceil(length / 2)
 	print("image info: w: "..width.." l: "..length.." format: "..format)
-	print("image origin: x: "..center_x.." z: "..center_z)
 	print("after tiff offset: "..offset)
 end
 
@@ -19,7 +16,7 @@ end
 
 -- Parameters
 
-local VERSCA = 30 -- Vertical scale, meters per node
+local VERSCA = 10 -- Vertical scale, meters per node
 local YWATER = 1
 
 -- Set mapgen parameters
@@ -39,6 +36,8 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	local y0 = minp.y
 	local z0 = minp.z
 
+	local blelev = get_pixel(x0, z0)
+	print("block corner elev: "..blelev)
 	local sidelen = x1 - x0 + 1
 	local ystridevm = sidelen + 32
 
@@ -58,7 +57,7 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	--local blockel = get_pixel(x0, z0)
 	for z = z0, z1 do
 	for x = x0, x1 do
-		local elev = get_pixel(2*x, 2*z) / 100 -- elevation in meters from DEM 
+		local elev = get_pixel(x, z) -- elevation in meters from DEM 
 				-- use demi to get elevation value from flat array
 		local node_elev = math.floor(YWATER + elev / VERSCA)
 		local vi = area:index(x, y0, z) -- voxelmanip index
@@ -86,26 +85,36 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	vm:update_liquids()
 
 	local chugent = math.ceil((os.clock() - t0) * 1000)
-	print ("[DEM] "..chugent.." ms  mapchunk ("..cx0..", "..cz0..")")
+	print ("[DEM] "..chugent.." ms  mapchunk ("..cx0..", "..math.floor((y0 + 32) / 80)..", "..cz0..")")
 end)
 
 --for now we are going to assume 32 bit signed elevation pixels
 --and a header offset of
 
 function get_pixel(x,z)
-	local row = center_z + z
-	local col = center_x + x
+	if x > math.ceil(width / 2) or x < - math.floor(width / 2)
+	or z > math.ceil(length / 2) or z < - math.floor(length / 2) then
+		--print ("out of range of tiff")
+		return 0 --off the TIFF,
+	end
+	
+	local row = math.floor(length / 2) + 2*z --having to discard every other row for some reason
+	local col = width + (2 * x) --width = middle byte since each pixel is two bytes
 	--print("bitmap row: "..row.." col: "..col)
-	demtiff:seek("set", ( offset + (row * width) + col ))
-	local x = bytes_to_int(demtiff:read(2):byte(1,2))
-	--print ("block elev: "..x.."m")
-	return x
+	demtiff:seek("set", ( offset + (row * (width+1)) + col ))
+	local twobytes = demtiff:read(2)
+	--print(twobytes)
+	local a,b = twobytes:byte(1,2)
+	--print ("a: "..a..", b: "..b)
+	local el = bytes_to_int(a,b)
+	--print ("block el: "..el.."m")
+	return el -800
 end
 
 -- convert bytes (little endian) to a 32-bit two's complement integer
 function bytes_to_int(b1, b2 )--b3, b4)
   if not b2 then error("need two bytes to convert to int",2) end
   local n = b1 + b2*256 --+ b3*65536 + b4*16777216
-  n = (n > 32768) and (n - 65536) or n
+  --n = (n > 32768) and (n - 65536) or n -- no negatives in this dem
   return n
 end

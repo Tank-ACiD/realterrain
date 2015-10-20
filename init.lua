7e8af750cac84415877af9d6732885a12a428006
Bob Omb <bobombolo@omb.net>
2015-10-20 15:27:31 -0700

    biome, water and road painting, plus settings in menu

--- init.lua
+++ init.lua
@@ -1,30 +1,34 @@
+MODPATH = minetest.get_modpath("realterrain")
+WORLDPATH = minetest.get_worldpath() 
 local realterrain = {}
+realterrain.settings = {}
+
 local ie = minetest.request_insecure_environment()
 ie.require "luarocks.loader"
-local imlib2 = ie.require "imlib2"
 local magick = ie.require "magick"
 
--- Parameters
-local DEM = 'mandelbrot16bit.tif'
-local COVER = 'cover.tif' --cover should only be an 8-bit file of the same dimensions as the DEM
-
-realterrain.settings = {} --form persistence
-
 --defaults
-realterrain.settings.bits = 8
+realterrain.settings.bits = 8 --@todo remove this setting when magick autodetects bitdepth
 realterrain.settings.yscale = 1
 realterrain.settings.xscale = 1
 realterrain.settings.zscale = 1
-realterrain.settings.waterlevel = 1
+realterrain.settings.waterlevel = 0
 realterrain.settings.alpinelevel = 200
+realterrain.settings.filedem   = 'dem.tif'
+realterrain.settings.filewater = 'water.tif'
+realterrain.settings.fileroads = 'roads.tif'
+realterrain.settings.filebiome = 'biomes.tif'
 
-local demfilename = minetest.get_modpath("realterrain").."/dem/"..DEM
-local dem = magick.load_image(demfilename)
+--@todo fail if there is no DEM?
+local dem = magick.load_image(MODPATH.."/dem/"..realterrain.settings.filedem)
 local width = dem:get_width()
 local length = dem:get_height()
-print("width: "..width..", height: "..length)
-local coverfilename = minetest.get_modpath("realterrain").."/dem/"..COVER
-local cover = imlib2.image.load(coverfilename)
+--print("width: "..width..", height: "..length)
+local biomeimage, waterimage, roadimage
+biomeimage = magick.load_image(MODPATH.."/dem/"..realterrain.settings.filebiome)
+waterimage = magick.load_image(MODPATH.."/dem/"..realterrain.settings.filewater)
+roadimage  = magick.load_image(MODPATH.."/dem/"..realterrain.settings.fileroads)
+--@todo throw warning if image sizes do not match the dem size
 
 -- Set mapgen parameters
 
@@ -42,9 +46,29 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	local x0 = minp.x
 	local y0 = minp.y
 	local z0 = minp.z
-
-	--local blelev = get_pixel(x0, z0)
-	--print("block corner elev: "..blelev)
+	
+	--content ids
+	local c_grass  = minetest.get_content_id("default:dirt_with_grass")
+	local c_gravel = minetest.get_content_id("default:gravel")
+	local c_stone  = minetest.get_content_id("default:stone")
+	local c_sand   = minetest.get_content_id("default:sand")
+	local c_water  = minetest.get_content_id("default:water_source")
+	local c_dirt   = minetest.get_content_id("default:dirt")
+	local c_coal   = minetest.get_content_id("default:stone_with_coal")
+	local c_cobble = minetest.get_content_id("default:cobble")
+	--biome specific cids
+	local cids = {}
+	cids[1]  = {grass=minetest.get_content_id("wool:black")}
+	cids[2]  = {grass=minetest.get_content_id("wool:brown")}
+	cids[3]  = {grass=minetest.get_content_id("wool:dark_green")}
+	cids[4]  = {grass=minetest.get_content_id("wool:purple")}
+	cids[5]  = {grass=minetest.get_content_id("wool:dark_grey")}
+	cids[6]  = {grass=minetest.get_content_id("wool:blue")}
+	cids[7]  = {grass=minetest.get_content_id("wool:green")}
+	cids[8]  = {grass=minetest.get_content_id("wool:red")}
+	cids[9]  = {grass=minetest.get_content_id("wool:orange")}
+	cids[10] = {grass=minetest.get_content_id("wool:yellow")}
+	
 	local sidelen = x1 - x0 + 1
 	local ystridevm = sidelen + 32
 
@@ -54,62 +78,60 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
 	local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
 	local data = vm:get_data()
-	
-	local c_grass  = minetest.get_content_id("default:dirt_with_grass")
-	local c_gravel = minetest.get_content_id("default:gravel")
-	local c_stone  = minetest.get_content_id("default:stone")
-	local c_sand   = minetest.get_content_id("default:sand")
-	local c_water  = minetest.get_content_id("default:water_source")
-	local c_dirt   = minetest.get_content_id("default:dirt")
-	local c_coal   = minetest.get_content_id("default:stone_with_coal")
-	local c_cobble = minetest.get_content_id("default:cobble")
-
-	local demi = 1 -- index of 80x80 flat array of DEM values
-	--local blockel = get_pixel(x0, z0)
 	for z = z0, z1 do
 	for x = x0, x1 do
-		local elev, cover = realterrain.get_pixel(x, z) -- elevation in meters from DEM and water true/false
-				-- use demi to get elevation value from flat array
-
+		local elev, biome, water, road = realterrain.get_pixel(x, z) -- elevation in meters from DEM and water true/false
+		--print("elev: "..elev..", biome: "..biome..", water: "..water..", road: "..road)
+		
 		local vi = area:index(x, y0, z) -- voxelmanip index
 		for y = y0, y1 do
             --underground layers
 			if y < elev then 
 				--create strata of stone, cobble, gravel, sand, coal, iron ore, etc
-				if y < elev - 35 then
+				if y < elev - (30 + math.random(1,5)) then
 					data[vi] = c_sand
-				elseif y < elev - 30 then
+				elseif y < elev - (25 + math.random(1,5)) then
 					data[vi] = c_gravel
-				elseif y < elev - 25 then
+				elseif y < elev - (20 + math.random(1,5)) then
 					data[vi] = c_stone
-				elseif y < elev - 20 then
+				elseif y < elev - (15 + math.random(1,5)) then
 					data[vi] = c_coal
-				elseif y < elev - 15 then
+				elseif y < elev - (10 + math.random(1,5)) then
 					data[vi] = c_stone
-				elseif y < elev - 10 then
+				elseif y < elev - (5 + math.random(1,5)) then
 					data[vi] = c_gravel
 				else
 					data[vi] = c_dirt
 				end
-			--the surface layer, determined by "cover"
+			--the surface layer, determined by the different cover files
 			elseif y == elev then
 				--rivers
-				if cover > 225 then
-					data[vi] = c_water
-				 --roads
-				elseif cover > 99 then
+				if road > 0 then
 					data[vi] = c_cobble
-				--biome cover (elev based for now)
+				 --roads
+				elseif water > 0 then
+					data[vi] = c_water
+				--biome cover
 				else
 					--sand for lake bottoms
 					if y < tonumber(realterrain.settings.waterlevel) then
 						data[vi] = c_sand
 					--alpine level
-					elseif y > tonumber(realterrain.settings.alpinelevel) then 
+					elseif y > tonumber(realterrain.settings.alpinelevel) + math.random(1,5) then 
 						data[vi] = c_gravel
 					--default
 					else
-						data[vi] = c_grass
+						if biome < 10  then data[vi] = cids[1].grass
+						elseif biome < 20  then data[vi] = cids[2].grass 
+						elseif biome < 30  then data[vi] = cids[3].grass
+						elseif biome < 40  then data[vi] = cids[4].grass
+						elseif biome < 50  then data[vi] = cids[5].grass
+						elseif biome < 60  then data[vi] = cids[6].grass
+						elseif biome < 70  then data[vi] = cids[7].grass
+						elseif biome < 80  then data[vi] = cids[8].grass
+						elseif biome < 90  then data[vi] = cids[9].grass
+						else   data[vi] = cids[10].grass
+						end
 					end
 				end
 			elseif y <= tonumber(realterrain.settings.waterlevel) then
@@ -117,7 +139,6 @@ minetest.register_on_generated(function(minp, maxp, seed)
 			end
 			vi = vi + ystridevm
 		end
-		demi = demi + 1
 	end
 	end
 	
@@ -134,25 +155,27 @@ end)
 --and a header offset of
 
 function realterrain.get_pixel(x,z)
-
+	local e, b, w, r = 0,0,0,0
     local row,col = 0-z, 0+x
-	--adjust for scales
+	--adjust for x and z scales
     row = math.floor(row / tonumber(realterrain.settings.zscale))
     col = math.floor(col / tonumber(realterrain.settings.xscale))
     
-    --off the dem return zero
-    if ((col < 0) or (col > width) or (row < 0) or (row > length)) then return 0,0 end
+    --off the dem return zero for all values
+    if ((col < 0) or (col > width) or (row < 0) or (row > length)) then return 0,0,0,0 end
     
-    
-    local elev = dem:get_pixel(col, row)
+    e = dem:get_pixel(col, row)
     --print(elev)
-    local cover = cover:get_pixel(col, row)
+	if biomeimage then b = 100 * biomeimage:get_pixel(col, row) end--use breakpoints for different biomes
+	if waterimage then w = math.ceil(waterimage:get_pixel(col, row)) end --@todo use float for water depth?
+	if roadimage  then r = math.ceil(roadimage:get_pixel(col, row)) end --@todo use breakpoints for building height?
 	
     --adjust for bit depth and vscale
-    elev = math.floor(elev * (2^tonumber(realterrain.settings.bits)))
-    elev = math.floor(elev / tonumber(realterrain.settings.yscale))
-    --print(elev)
-    return elev, cover.red
+    e = math.floor(e * (2^tonumber(realterrain.settings.bits))) --@todo change when magick autodetects bit depth
+    e = math.floor(e / tonumber(realterrain.settings.yscale))
+    
+	--print("elev: "..e..", biome: "..b..", water: "..w..", road: "..r)
+    return e, b, w, r
 end
 
 -- the controller for changing map settings
@@ -171,7 +194,7 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 	if string.sub(formname, 1, 12) == "realterrain:" then
 		local wait = os.clock()
 		while os.clock() - wait < 0.05 do end --popups don't work without this see issue #30
-		print("fields submitted: "..dump(fields))
+		--print("fields submitted: "..dump(fields))
 		local pname = player:get_player_name()
 		
 		-- always save any form fields
@@ -211,7 +234,7 @@ end)
 
 --called at each form submission
 function realterrain.save_settings()
-	local file = io.open(minetest.get_worldpath().."/realterrain_settings", "w")
+	local file = io.open(WORLDPATH.."/realterrain_settings", "w")
 	if file then
 		for k,v in next, realterrain.settings do
 			local line = {key=k, values=v}
@@ -222,7 +245,7 @@ function realterrain.save_settings()
 end
 -- load settings run at EOF at mod start
 function realterrain.load_settings()
-	local file = io.open(minetest.get_worldpath().."/realterrain_settings", "r")
+	local file = io.open(WORLDPATH.."/realterrain_settings", "r")
 	if file then
 		for line in file:lines() do
 			if line ~= "" then
@@ -278,7 +301,16 @@ function realterrain.show_rc_form(pname)
 								"field[1,5;4,1;waterlevel;Water Level;"..
                                     minetest.formspec_escape(realterrain.get_setting("waterlevel")).."]"..
                                 "field[1,6;4,1;alpinelevel;Alpine Level;"..
-                                    minetest.formspec_escape(realterrain.get_setting("alpinelevel")).."]"
+                                    minetest.formspec_escape(realterrain.get_setting("alpinelevel")).."]"..
+									
+								"field[6,3;4,1;filedem;Elevation File;"..
+                                    minetest.formspec_escape(realterrain.get_setting("filedem")).."]" ..
+								"field[6,4;4,1;filebiomes;Biome File;"..
+                                    minetest.formspec_escape(realterrain.get_setting("filebiome")).."]" ..
+								"field[6,5;4,1;filewater;Water File;"..
+                                    minetest.formspec_escape(realterrain.get_setting("filewater")).."]"..
+                                "field[6,6;4,1;fileroads;Road File;"..
+                                    minetest.formspec_escape(realterrain.get_setting("fileroads")).."]"
 	--Action buttons
 	local f_footer = 			"label[3,8.5;Delete the map, reset]"..
 								"button_exit[3,9;2,1;exit;Delete]"..
